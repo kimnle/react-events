567b8c6ee7603629ab450d8ef982a9787e1c41af
Kim <72332310+kimnle@users.noreply.github.com>
2024-09-17 20:51:59 +1000

    feat: pokemon retrieval

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './App.css';
+import PokemonCard from "./components/PokemonCard";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      numberOfPokemon: 6
+    }
+  }
+
+  exmapleLog1() {
+    console.log("Function 1 logging!!");
+  }
+
+  exampleLog2() {
+    console.log("Function 2 logging!!");
+  }
+
+  decreasePokemonNumber() {
+    if (this.state.numberOfPokemon > 0) {
+      this.setState({numberOfPokemon: this.state.numberOfPokemon - 1});
+    }
+  }
+
+  // decreasePokemonNumber = () => {
+  //   this.setState({numberOfPokemon: this.state.numberOfPokemon - 1});
+  // };
+
+  increasePokemonNumber = () => {
+    this.setState({numberOfPokemon: this.state.numberOfPokemon + 1});
+  }
+
+  render() {
+    return(
+      <>
+      <h1>Hello World!!</h1>
+      <p>Blah blah blah blah</p>
+      <h2>Number of Pokemon: {this.state.numberOfPokemon}</h2>
+      <button onClick={() => {
+        this.exmapleLog1();
+        this.exampleLog2();
+        this.decreasePokemonNumber();
+      }}>
+        Decrease Pokemon Amount
+      </button>
+      <button onClick={this.increasePokemonNumber}>
+        Increase Pokemon Amount
+      </button>
+
+      {
+        // Create an array with a length equal to state
+        Array(this.state.numberOfPokemon)
+        .fill(null) // fill() just gives each item in the array something, so we can use it properly
+        // the actual part that does the loop or repeated action
+        .map((element, index) => {
+          // repeated action is in this block!!
+          return <PokemonCard key={index} />
+        })
+      }
+
+      </>
+    )
+  }
+}
+
+export default App;
